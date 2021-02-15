8f9be048246faa1585913ed98da132ff55f3ea45
Jason Ozias <jason.g.ozias@gmail.com>
2021-02-15 14:31:41 -0500

    Example 2 - Move (#2)
    * work
    
    * example 2
    
    Co-authored-by: Jason Ozias <jason.ozias@kroger.com>

--- src/ex01.rs
+++ src/ex01.rs
@@ -1,28 +1,26 @@
-use slog::{info, Logger};
-
-crate fn variable_scope(stdout: &Logger) {
+crate fn ex01_scope() {
     {
         // 'a_str' is not valid here, it’s not yet declared
         //
-        // Uncomment line 20 to see the following error:
+        // Uncomment line 18 to see the following error:
         //
         // error[E0425]: cannot find value `a_str` in this scope
-        //   --> src/ex01.rs:20:38
+        //   --> src/ex01.rs:18:33
         //    |
-        // 20 |         info!(stdout, "a_str is {}", a_str);
-        //    |                                      ^^^^^ not found in this scope
+        // 17 |         println!("a_str is {}", a_str);
+        //    |                                 ^^^^^ not found in this scope
         //
         // error: aborting due to previous error
         //
         // For more information about this error, try `rustc --explain E0425`.
         // error: could not compile `rust_own`
         //
-        // info!(stdout, "a_str is {}", a_str);
+        // println!("a_str is {}", a_str);
 
-        let a_str = "trust-rust";               // 'a_str' is valid from this point forward
-        info!(stdout, "a_str is {}", a_str);         // You can use 'a_str' while it is in scope
-    }                                                // This curly brace indicates that our scope is now over, and 'a_str' is no longer valid
+        let a_str = "trust-rust"; // 'a_str' is valid from this point forward
+        println!("a_str is {}", a_str); // You can use 'a_str' while it is in scope
+    } // This curly brace indicates that our scope is now over, and 'a_str' is no longer valid
 
-    // Uncomment line 27 to see a similar scope error to the above
-    // info!(stdout, "a_str is {}", a_str);
+    // Uncomment line 25 to see a similar scope error to the above
+    // println!("a_str is {}", a_str);
 }
