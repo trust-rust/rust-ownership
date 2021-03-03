afd2bb3c379b6bd13bbacee62a62d7c6b0ef2aa7
Jason Ozias <jason.ozias@kroger.com>
2021-03-03 09:05:52 -0500

    missed old function call

--- src/ex08.rs
+++ src/ex08.rs
@@ -2,7 +2,6 @@ crate fn ex08_mutable_reference() {
     let mut x = String::from("try to change me");
     change_it(&mut x);
     println!("x is '{}'", x);
-    blah();
 }
 
 fn change_it(x: &mut String) {
